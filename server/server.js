d6ee161db9843d85f5f2a3bd0e66b154c1ed06c1
Maida Israr <maidaisrar19@gmail.com>
2026-01-18 13:58:16 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,37 +1,33 @@
 import express from "express";
 import cors from "cors";
-import 'dotenv/config';
+import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
-import authRouter from './routes/authRoutes.js'
+import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 
 const app = express();
-const port = process.env.PORT || 4000
- connectDB();  
- 
+const port = process.env.PORT || 4000;
 
-   
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
- 
 
-const cors = require("cors");
+app.use(
+  cors({
+    origin: [
+      "https://mern-authentication-66zg.vercel.app",
+      "https://mern-authentication-66zg-c6946achq-maidas-projects-455c8012.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 
-app.use(cors({
-  origin: [
-    "https://mern-authentication-66zg.vercel.app",
-    "https://mern-authentication-66zg-c6946achq-maidas-projects-455c8012.vercel.app"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true
-}));
+// API Endpoints
+app.get("/", (req, res) => res.send("API is Working."));
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-//API Endpoints
-app.get('/',(req,res)=> res.send("API is Working."));
-app.use('/api/auth', authRouter)
-app.use('/api/user', userRouter)
-
-app.listen(port,()=>console.log(` Server started on PORT:${port} `));
-  
+app.listen(port, () => console.log(`Server started on PORT:${port}`));
